3e8b12652777ead38e551c42134788227d8e18e3
Mayank Dubey <mayank.dubey@appinventiv.com>
2024-02-19 17:12:25 +0530

    first commit

---
+++ lib/mybottombar.dart
@@ -0,0 +1,53 @@
+library mybottombar;
+
+import 'package:flutter/material.dart';
+
+class MyBottomBar extends StatefulWidget {
+  final List<NavBarItem> items;
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+  final Color? iconColor;
+  final Color? labelColor;
+
+  const MyBottomBar({
+    Key? key,
+    required this.items,
+    required this.currentIndex,
+    required this.onTap,
+    this.iconColor = Colors.blue,
+    this.labelColor = Colors.blue,
+  }) : super(key: key);
+
+  @override
+  MyBottomBarState createState() => MyBottomBarState();
+}
+
+class MyBottomBarState extends State<MyBottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: widget.items
+          .map((item) => BottomNavigationBarItem(
+        icon: Icon(item.icon),
+        label: item.label,
+      ))
+          .toList(),
+      currentIndex: widget.currentIndex,
+      selectedItemColor: widget.iconColor,
+      unselectedItemColor: Colors.grey,
+      onTap: widget.onTap,
+      selectedLabelStyle: TextStyle(color: widget.labelColor),
+      unselectedLabelStyle:const TextStyle(color: Colors.grey),
+    );
+  }
+}
+
+class NavBarItem {
+  final IconData icon;
+  final String label;
+
+  NavBarItem({
+    required this.icon,
+    required this.label,
+  });
+}
